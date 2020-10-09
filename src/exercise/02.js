03e11da404484cece35207a665dc5536f326ed1f
Samuel Pitoňák <Samuel.Pitonak@senacor.com>
2020-10-09 16:09:28 +0200

    Add solutions for exercise 1 & 2

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -1,12 +1,24 @@
 // useEffect: persistent state
 // http://localhost:3000/isolated/exercise/02.js
 
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+function useLocalStorageState(key, initialValue = '') {
+  const [value, setValue] = useState(
+    () => window.localStorage.getItem(key) || initialValue,
+  )
+
+  useEffect(() => {
+    window.localStorage.setItem(key, value)
+  }, [key, value])
+
+  return [value, setValue]
+}
 
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') || initialName
-  const [name, setName] = React.useState(initialName)
+  const [name, setName] = useLocalStorageState('name', initialName);
 
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
@@ -15,11 +27,12 @@ function Greeting({initialName = ''}) {
   function handleChange(event) {
     setName(event.target.value)
   }
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
