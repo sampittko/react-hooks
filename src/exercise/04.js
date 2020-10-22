d8f4de2a6c68601cd8a942e72a0a3c4cbf02d53e
Samuel Pitoňák <Samuel.Pitonak@senacor.com>
2020-10-22 12:59:46 +0200

    Add exercise 4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -1,42 +1,13 @@
 // useState: tic tac toe
 // http://localhost:3000/isolated/exercise/04.js
 
-import React from 'react'
+import React, {useState} from 'react'
+import {useLocalStorageState} from '../utils'
 
-function Board() {
-  // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
-  // 🐨 We'll need the following bits of derived state:
-  // - nextValue ('X' or 'O')
-  // - winner ('X', 'O', or null)
-  // - status (`Winner: ${winner}`, `Scratch: Cat's game`, or `Next player: ${nextValue}`)
-  // 💰 I've written the calculations for you! So you can use my utilities
-  // below to create these variables
-
-  // This is the function your square click handler will call. `square` should
-  // be an index. So if they click the center square, this will be `4`.
-  function selectSquare(square) {
-    // 🐨 first, if there's already winner or there's already a value at the
-    // given square index (like someone clicked a square that's already been
-    // clicked), then return early so we don't make any state changes
-    //
-    // 🦉 It's typically a bad idea to manipulate state in React because that
-    // can lead to subtle bugs that can easily slip into productions.
-    // 🐨 make a copy of the squares array (💰 `[...squares]` will do it!)
-    // 🐨 Set the value of the square that was selected
-    // 💰 `squaresCopy[square] = nextValue`
-    //
-    // 🐨 set the squares to your copy
-  }
-
-  function restart() {
-    // 🐨 set the squares to `Array(9).fill(null)`
-  }
-
+function Board({squares, onClick}) {
   function renderSquare(i) {
     return (
-      <button className="square" onClick={() => selectSquare(i)}>
+      <button className="square" onClick={() => onClick(i)}>
         {squares[i]}
       </button>
     )
@@ -44,8 +15,6 @@ function Board() {
 
   return (
     <div>
-      {/* 🐨 put the status here */}
-      <div className="status">STATUS</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -61,18 +30,70 @@ function Board() {
         {renderSquare(7)}
         {renderSquare(8)}
       </div>
-      <button className="restart" onClick={restart}>
-        restart
-      </button>
     </div>
   )
 }
 
 function Game() {
+  const [history, setHistory] = useLocalStorageState('history', [
+    Array(9).fill(null),
+  ])
+
+  const [currentStep, setCurrentStep] = useState(history.length)
+
+  const currentSquares = history[currentStep - 1]
+
+  const nextValue = calculateNextValue(currentSquares)
+  const winner = calculateWinner(currentSquares)
+  const status = calculateStatus(winner, currentSquares, nextValue)
+
+  const moves = history.map((historyItem, i) => {
+    const isCurrent = currentStep - 1 === i
+
+    return (
+      <li key={`history-item-${i}`}>
+        <button
+          disabled={isCurrent}
+          onClick={() => setCurrentStep(i + 1)}
+          children={!isCurrent ? `Go to step #${i + 1}` : 'Current step'}
+        />
+      </li>
+    )
+  })
+
+  function selectSquare(square) {
+    if (winner || currentSquares[square]) {
+      return
+    }
+
+    const squaresCopy = [...currentSquares]
+
+    squaresCopy[square] = nextValue
+
+    if (currentStep !== history.length) {
+      setHistory([...history.slice(0, currentStep), squaresCopy])
+    } else {
+      setHistory([...history, squaresCopy])
+    }
+    setCurrentStep(prevStep => prevStep + 1)
+  }
+
+  const restart = () => {
+    setHistory([Array(9).fill(null)])
+    setCurrentStep(1)
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board onClick={selectSquare} squares={currentSquares} />
+        <button className="restart" onClick={restart}>
+          restart
+        </button>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <ol>{moves}</ol>
       </div>
     </div>
   )
